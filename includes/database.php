e07e599bc9aaa21aff084e41f48667ea8ddc50b0
kranthi <kranthi610@gmail.com>
2017-11-09 00:11:21 -0600

    connections2

--- includes/database.php
+++ includes/database.php
@@ -8,11 +8,11 @@ $password = "";
 $dbname = "chat";
 */
 
-$servername = "herokuapp.com";
+$servername = "herokuapp";
 $username = "b2c29ca89ee067";
 $password = "4c4cec06";
 $dbname = "heroku_4b46c901d7863ba";
-
+echo " notconnected";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
