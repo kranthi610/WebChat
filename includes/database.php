220de193fff5b7ec6e8d169e4acba29bf81e3c63
kranthi <kranthi610@gmail.com>
2017-11-09 00:19:43 -0600

    connections2

--- includes/database.php
+++ includes/database.php
@@ -8,7 +8,7 @@ $password = "";
 $dbname = "chat";
 */
 
-$servername = "localhost";
+$servername = "mysql://b2c29ca89ee067:4c4cec06@us-cdbr-iron-east-05.cleardb.net/heroku_4b46c901d7863ba";
 $username = "b2c29ca89ee067";
 $password = "4c4cec06";
 $dbname = "heroku_4b46c901d7863ba";
